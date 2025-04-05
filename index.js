bc39d7626bbf01e0d62c799ea575bb159bd5eda2
Sahilmaliya88 <sahilmaliya55@gmail.com>
2025-04-05 16:18:37 +0530

    feature:fixed bug

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.use((req,res,next)=>{
 })
 // server
 app.get("/", (request, response) => {
-  response.send(`<h1>Hello from ${request.session.user.username}</h1>`);
+  response.send("hello from sahil")
 });
 
 // routers
